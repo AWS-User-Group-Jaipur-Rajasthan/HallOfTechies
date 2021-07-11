b18cfb0f6d38dd4cf3aed81fb11fbb916704038f
aksh-22 <akashkhandelwal1999@gmail.com>
2021-07-11 23:11:00 +0530

    change

--- src/upload/data.js
+++ src/upload/data.js
@@ -10,6 +10,17 @@ const data = [
       "https://twitter.com",
     ],
   },
+  {
+    id: 7002,
+    name: "Akash",
+    role: "MERN Stack Developer",
+    image: "https://avatars.githubusercontent.com/u/56211500?v=4",
+    link: [
+      "https://github.com/aksh-22",
+      "https://linkedin.com",
+      "https://twitter.com",
+    ],
+  },
 ];
 
 export default data;
