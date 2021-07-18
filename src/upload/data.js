8610b76390b4f954022918b75c37774beea39a69
Samarth Agarwal <asamarth70@gmail.com>
2021-07-19 00:49:36 +0530

    Update data.js

--- src/upload/data.js
+++ src/upload/data.js
@@ -55,17 +55,6 @@ const data = [
     ],
   },
   {
-    name: "Samarth Agarwal",
-    role: "Software Developer",
-    image: "https://avatars.githubusercontent.com/u/44058680?v=4",
-    skills: ['Java','Python','Cloud Technologies','RestAPIs'],
-    link: [
-      "https://github.com/Thecapable",
-      "https://www.linkedin.com/in/samarth-agarwal-6b9204171/",
-      "https://twitter.com/The_Capable1",
-    ],
-  },
-  {
     name: "Muhammad Sami Khanday",
     role: "CS Undergrad",
     image: "https://avatars.githubusercontent.com/u/61789670?v=4",
@@ -76,6 +65,17 @@ const data = [
       "https://twitter.com/msamik7",
     ],
   },
+  {
+    name: "Samarth Agarwal",
+    role: "Software Developer",
+    image: "https://avatars.githubusercontent.com/u/44058680?v=4",
+    skills: ['Java','Python','Cloud Technologies','RestAPIs'],
+    link: [
+      "https://github.com/Thecapable",
+      "https://www.linkedin.com/in/samarth-agarwal-6b9204171/",
+      "https://twitter.com/The_Capable1",
+    ],
+  }
 ];
 
 export default data;
