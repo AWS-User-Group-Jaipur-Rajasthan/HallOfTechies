13bc642639fc95a0c7b9dd25ba7a6e37689dea67
DEEPAK KUMAR <78249949+DK85690@users.noreply.github.com>
2021-07-19 11:11:53 +0530

    Add files via upload

---
+++ Deepak.data.js
@@ -0,0 +1,80 @@
+const data = [
+  {
+    name: "Akash Khandelwal",
+    role: "MERN Stack Developer",
+    image: "https://avatars.githubusercontent.com/u/56211500?v=4",
+    skills: ['js', 'react', 'node', 'react native'],
+    link: [
+      "https://github.com/aksh-22",
+      "https://linkedin.com",
+      "https://twitter.com",
+    ],
+  },
+  {
+    name: "Ankit Chaudhari",
+    role: "Full Stack Developer",
+    image: "https://avatars.githubusercontent.com/u/65450600?v=4",
+    skills: ['html', 'css', 'js', 'docker'],
+    link: [
+      "https://github.com/Ankit1598",
+      "https://www.linkedin.com/in/ankit1598/",
+      "https://twitter.com/Ankit15_",
+    ],
+  },
+  {
+    name: "Priyanka Prasad",
+    role: "Postman Student Expert | Developer",
+    image: "https://avatars.githubusercontent.com/u/59612128?s=400&u=508b3cb2b19845ebf3a38aec415d0e455922a816&v=4",
+    skills: ['html', 'css', 'js', 'docker','api'],
+    link: [
+      "https://github.com/P-riyanka-prasad",
+      "https://www.linkedin.com/in/priyanka677/",
+      "https://twitter.com/Priyank43692137",
+    ],
+  },
+  {
+    name: "Palash Burad",
+    role: "Full Stack Developer",
+    image: "https://avatars.githubusercontent.com/u/36355091?v=4",
+    skills: ['html', 'css', 'js', 'docker'],
+    link: [
+      "https://github.com/palash9561",
+      "https://www.linkedin.com/in/palashburad26/",
+      "https://twitter.com/palashburad26",
+    ],
+  },
+  {
+    name: "Nishant Rajput",
+    role: "Technical Content Writer | Linux Enthusiast",
+    image: "https://avatars.githubusercontent.com/u/37553710?v=4",
+    skills: ['Bash', 'Python', 'Docker', 'JS', 'React','API'],
+    link: [
+      "https://github.com/nishant-rajput",
+      "https://www.linkedin.com/in/nishant-rajput",
+      "https://twitter.com/nishant_uwu",
+    ],
+  },
+  {
+    name: "Muhammad Sami Khanday",
+    role: "CS Undergrad",
+    image: "https://avatars.githubusercontent.com/u/61789670?v=4",
+    skills: ['devops','ml','mlops','fullstack'],
+    link: [
+      "https://github.com/m-samik",
+      "https://www.linkedin.com/in/m-samik/",
+      "https://twitter.com/msamik7",
+    ],
+  },
+  {
+    name: "Deepak Kumar",
+    role: "CSE Student",
+    image: "https://avatars.githubusercontent.com/u/78249949?v=4",
+    skills: ['html', 'css', 'js'],
+    link: [
+    "https://github.com/DK85690",
+    "https://twitter.com/DeepakK12286986",
+    ],
+    },
+];
+
+export default data;
