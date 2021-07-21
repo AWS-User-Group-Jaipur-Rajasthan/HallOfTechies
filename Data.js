0bf9cbc93eb8356b966dbcfff071323a48b27722
nlmupdy01 <80740629+nlmupdy01@users.noreply.github.com>
2021-07-21 22:17:42 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,12 @@
+{
+    name: "Neelam Upadhyay",
+    role: "Fullstack Developer , AWs Cloud, IT Security ",
+    image: "https://github.com/nlmupdy01.png ",
+    skills: ['HTML', 'css', 'js', 'python', 'cloud'],
+    link: [
+      "https://github.com/nlmupdy",
+      "https://linkedin.com/in/neelam-upadhyay-8736a3115",
+      "https://twitter.com/nlmupdy",
+    ],
+  }
+
