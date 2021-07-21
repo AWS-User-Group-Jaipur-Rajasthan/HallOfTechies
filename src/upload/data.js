d020410ac57fa2e4c8a02889f37a1beb9f00978d
Shashwat SIngh <55051478+Shashwatsingh22@users.noreply.github.com>
2021-07-21 11:03:26 +0530

    Update data.js

--- src/upload/data.js
+++ src/upload/data.js
@@ -749,6 +749,24 @@ const data = [
       " https://twitter.com/Divya82248572?s=08",
     ],
   }
+  
+  {
+    name: "Shashwat Singh",
+    role: "DevOps Intern | CSE Undergrad",
+    image: "https://avatars.githubusercontent.com/u/55051478?v=4",
+    skills: [
+  'DevOps-Tools',
+  'AWS-Cloud',
+  'MLOPS', 
+  'CCNA-Networking',
+  'Flutter'
+],
+    link: [
+      "https://github.com/Shashwatsingh22",
+      "https://linkedin.com/in/shashwatsing/",
+      "https://twitter.com/TWITTER_USERNAME",
+    ],
+  }
 
 ];
 
