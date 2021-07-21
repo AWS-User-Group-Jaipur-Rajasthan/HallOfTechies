b620121e636cad5b13b1fefdd5a72fbee4df4408
nlmupdy01 <80740629+nlmupdy01@users.noreply.github.com>
2021-07-21 23:23:02 +0530

    Update Data.js

--- Data.js
+++ Data.js
@@ -2,7 +2,7 @@
     name: "Neelam Upadhyay",
     role: "Fullstack Developer , AWs Cloud, IT Security ",
     image: "https://github.com/nlmupdy01.png ",
-    skills: ['HTML', 'css', 'js', 'python', 'cloud', 'cyber security'],
+    skills: ['HTML', 'css', 'js', 'python', 'cloud', 'cybersecurity'],
     link: [
       "https://github.com/nlmupdy01",
       "https://linkedin.com/in/neelam-upadhyay-8736a3115",
