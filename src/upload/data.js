391acf15501ef2a559f51c93c9d014d48da65deb
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-07-21 17:32:09 +0530

    Added skills and social links

--- src/upload/data.js
+++ src/upload/data.js
@@ -824,11 +824,11 @@ const data = [
     name: "Riya Gupta",
     role: "Frontend Developer",
     image: "https://avatars.githubusercontent.com/u/72425181?s=400&u=9e732e1c9ecf6f4b0a0c4ddad76984d506963f60&v=4",
-    skills: ['js', 'react', 'node', 'react native'],
+    skills: ['HTML5', 'CSS3', 'JS', 'React', 'NodeJS', 'ExpressJS', 'MongoDB', 'Mongoose', 'C++'],
     link: [
-      "https://github.com/GITHUB_USERNAME",
-      "https://linkedin.com/in/LINKEDIN_USERNAME",
-      "https://twitter.com/TWITTER_USERNAME",
+      "https://github.com/RiyaGupta89",
+      "https://www.linkedin.com/in/riyaaa",
+      "https://twitter.com/Riya54791008",
     ],
   }
 
