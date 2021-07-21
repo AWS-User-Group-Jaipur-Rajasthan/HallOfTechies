3a98ff8d0484f954037a1ee75e2e95d5ddeb0bf8
Saurav Kumar <87579538+saurav9283@users.noreply.github.com>
2021-07-21 12:26:12 +0530

    Update data.js

--- src/upload/data.js
+++ src/upload/data.js
@@ -748,7 +748,24 @@ const data = [
       "https://www.linkedin.com/in/divya-jainn/",
       " https://twitter.com/Divya82248572?s=08",
     ],
-  }
+    
+    
+    {
+    name: "Saurav kumar",
+    role: "B.Tech CSE Student at Lovely professional university",
+    image: "https://avatars.githubusercontent.com/u/87579538?v=4",
+    skills: ['C', 'C++', 'Python'],
+    link: [
+      "https://github.com/saurav9283",
+      "https://www.linkedin.com/feed/",
+      "https://twitter.com/home?lang=en",
+    ],
+    }
+
+
+  
+  
+  
 
 ];
 
